55004c294cc89255a24b3e9904dd0f2689b10059
Aymane El-yamoun <ael-yamo@e2r11p2.1337.ma>
2022-05-18 15:04:35 +0100

    building token list

---
+++ source/list_manupulation.c
@@ -0,0 +1,34 @@
+#include "../includes/minishell.h"
+#include <unistd.h>
+
+token_t *new_token(unsigned int type, char *data)
+{
+	token_t *token;
+
+	token = malloc(sizeof(token_t));
+	token->type = type;
+	token->data = data;
+	token->next = NULL;
+	token->prev = NULL;
+}
+
+void	add_at_end(token_t *tokens_head, token_t *token)
+{
+	token_t	*tmp;
+
+	tmp = tokens_head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	token->prev = tmp;
+	tmp->next = token;
+}
+
+void	add_token_last(token_t *token_head, unsigned int type, char *data)
+{
+	if (token_head == NULL)
+		token_head = new_token(type, data);
+	else
+		add_at_end(token_head, new_token(type, data));
+}
