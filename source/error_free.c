84aff2acd05bee8ac8a71e41893c5ca4e50576b0
Raven <48260689+Oumaimafisaoui@users.noreply.github.com>
2022-05-26 12:27:42 +0100

    Add files via upload

---
+++ source/error_free.c
@@ -0,0 +1,27 @@
+#include "../includes/minishell.h"
+
+token_t *error_free(char *str, token_t *tokens)
+{
+    ft_putstr_fd("parse error near ", 2);
+    ft_putstr_fd("\'", 2);
+    ft_putstr_fd(str, 2);
+    ft_putstr_fd("\'", 2);
+	free_all(tokens);
+	return (NULL);
+}
+
+
+void	free_all(token_t *tokens)
+{
+	token_t	*tmp;
+
+	while (tokens)
+	{
+		tmp = tokens;
+		tokens = tokens->next;
+		free(tmp->data);
+		tmp->data = NULL;
+		free(tmp);
+		tmp = NULL;
+	}
+}
