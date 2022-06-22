152888973202e06caee0cf2739f951e4eab28361
Raven <48260689+Oumaimafisaoui@users.noreply.github.com>
2022-06-22 15:46:52 +0100

    echo fixed

--- source/builtins/ft_echo.c
+++ source/builtins/ft_echo.c
@@ -3,32 +3,37 @@
 void	ft_echo(char **str) 
 {
 	int index;
+	int flag = 0;
+	int j= 0;
 
 	index = 1;
 	while(str[index] && !ft_strcmp("-n", str[index]))
+	{
 		index++;
-	if(index > 1 && str[index])
-	{
-		ft_putstr_fd(str[index], 1);
+		flag = 1;
 	}
-	else if(index > 1 && !str[index])
-	{
+	if(index > 1 && !str[index])
 		ft_putchar_fd('\n', 1);
+	while(str[index] && !ft_strncmp("-n", str[index], 2))
+	{
+		while(str[index][j] == 'n')
+				j++;
+		while(str[index][j] == ' ')
+				j++;
+		index++;
+		flag = 1;
 	}
 	while(str[index])
 	{
 		if(ft_strcmp("$?", str[index]))
-		{
 			ft_putstr_fd(str[index], 1);
+		if(ft_strcmp(" ", str[index]))
 			ft_putchar_fd(' ', 1);
-		}
 		else
-		{
 			ft_putnbr_fd(gen->exit_status, 1);
-			ft_putchar_fd(' ', 1);
-		}
 		index++;
 	}
-	ft_putchar_fd('\n', 1);
+	if(flag == 0)
+		ft_putchar_fd('\n', 1);
 	gen->exit_status = 0;
 }
