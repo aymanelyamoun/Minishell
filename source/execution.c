d77fab46f2fac2b677554c34f816eda558f9a2d6
Aymane El-yamoun <ael-yamo@e3r9p6.1337.ma>
2022-06-30 19:45:04 +0100

    spliting the execution func

--- source/execution.c
+++ source/execution.c
@@ -51,9 +51,6 @@ char    *str_to_lower(char *str)
 
 void    execut(t_cmd *cmds, int **pipes, int pipes_num, int i)
 {
-	// char    *built_in;
-
-	// built_in = str_to_lower(cmds[i].cmd_args[0]);
 	dup2(cmds[i].infile, STDIN_FILENO);
 	dup2(cmds[i].outfile, STDOUT_FILENO);
 	close_pipes(pipes, pipes_num);
@@ -124,68 +121,96 @@ int is_buit_in(char *cmd)
 	}
 }
 
+void	exec_built_in(t_cmd *cmds, int i)
+{
+	int	in;
+	int	out;
+
+	in = dup(STDIN_FILENO);
+	out = dup(STDOUT_FILENO);
+	dup2(cmds[i].infile, STDIN_FILENO);
+	dup2(cmds[i].outfile, STDOUT_FILENO);
+	go_commands(cmds[i].cmd_args);
+	close(cmds[i].infile);
+	close(cmds[i].outfile);
+	dup2(in, STDIN_FILENO);
+	dup2(out, STDOUT_FILENO);
+	close(in);
+	close(out);
+}
+
+void	sig_ign(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	sig_dfl(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
+
+void	sig_handler(void)
+{
+	signal(SIGQUIT, handler);
+	signal(SIGINT, handler);
+}
+int		run_cmd(t_cmd *cmds, int **pipes, int pipes_num, int *i)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == -1)
+		perror("fork : ");
+	if (pid == 0)
+	{
+		sig_dfl();
+		execut(cmds, pipes, pipes_num, *i);
+	}
+	(*i)++;
+	return (pid);
+}
+
+void	wait_all(int pid, int i, int pipes_num)
+{
+	int	status;
+
+	if (i == pipes_num + 1)
+	{
+		waitpid(pid, &status, 0);
+	}
+	while (waitpid(-1, 0, 0) != -1)
+		;
+	gen.exit_status = WEXITSTATUS(status);
+	if (WIFSIGNALED(status))
+	{
+		if (WTERMSIG(status) == 3)
+			printf("Quit: 3\n");
+		gen.exit_status = 128 + WTERMSIG(status);
+	}
+}
+
 void    execution(t_cmd *cmds, int pipes_num)
 {
 	int i;
 	int pid;
 	int **pipes;
 	char *built_in;
-	int in;
-	int out;
-	int status;
 
 	i = 0;
-	signal(SIGINT, SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
 	pipes = creat_pipes(pipes_num);
 	assign_pipes(pipes, &cmds, pipes_num);
-
+	sig_ign();
 	if (cmds[i].exec == 0 && pipes_num == 0 && is_buit_in(cmds[i].cmd_args[0]))
-	{
-		in = dup(STDIN_FILENO);
-		out = dup(STDOUT_FILENO);
-		dup2(cmds[i].infile, STDIN_FILENO);
-		dup2(cmds[i].outfile, STDOUT_FILENO);
-		go_commands(cmds[i].cmd_args);
-		close(cmds[i].infile);
-		close(cmds[i].outfile);
-		dup2(in, STDIN_FILENO);
-		dup2(out, STDOUT_FILENO);
-		close(in);
-		close(out);
-	}
+		exec_built_in(cmds, i);
 	else
 	{
 		while (i <= pipes_num)
-		{
-			
-			pid = fork();
-			if (pid == -1)
-				perror("fork : ");
-			if (pid == 0)
-			{
-				signal(SIGINT, SIG_DFL);
-				signal(SIGQUIT, SIG_DFL);
-				execut(cmds, pipes, pipes_num, i);
-			}
-			i++;
-		}
+			pid = run_cmd(cmds, pipes, pipes_num, &i);
 		close_pipes(pipes, pipes_num);
-		if (i == pipes_num + 1)
-		{
-			waitpid(pid, &status, 0);
-		}
-		while (waitpid(-1, 0, 0) != -1)
-			;
-		gen.exit_status = WEXITSTATUS(status);
-		if (WIFSIGNALED(status))
-		{
-			if (WTERMSIG(status) == 3)
-				printf("Quit: 3\n");
-			gen.exit_status = 128 + WTERMSIG(status);
-		}
+		wait_all(pid, i, pipes_num);
 	}
 	free_pipes(pipes, pipes_num);
-	signal(SIGQUIT, handler);
-	signal(SIGINT, handler);
+	sig_handler();
 }
