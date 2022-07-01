e813a893085b6ab54b09221b114aeba111891369
Aymane El-yamoun <ael-yamo@e3r9p6.1337.ma>
2022-07-01 01:54:26 +0100

    small fix

---
+++ source/execution_utils.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-yamo <ael-yamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 01:51:46 by ael-yamo          #+#    #+#             */
+/*   Updated: 2022/07/01 01:53:15 by ael-yamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	close_cmd_files(t_cmd *cmds, int count)
+{
+	int	i;
+
+	i = 0;
+	while (i <= count)
+	{
+		close(cmds[i].infile);
+		close(cmds[i].outfile);
+		i++;
+	}
+}
+
+void	free_envp(void)
+{
+	int	i;
+
+	i = 0;
+	if (gen.envp != NULL)
+	{
+		while (gen.envp[i] != NULL)
+		{
+			free(gen.envp[i]);
+			i++;
+		}
+		free(gen.envp);
+	}
+}
+
+char	*str_to_lower(char *str)
+{
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	if (str != NULL)
+	{
+		new_str = malloc(sizeof(char) * ft_strlen(str) + 1);
+		while (str[i] != '\0')
+		{
+			new_str[i] = ft_tolower(str[i]);
+			i++;
+		}
+		new_str[i] = '\0';
+		return (new_str);
+	}
+	return (NULL);
+}
+
+void	free_cmds(t_cmd *cmds, int pipes_num)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (cmds != NULL)
+	{
+		while (i <= pipes_num)
+		{
+			j = 0;
+			if (cmds[i].cmd_args != NULL)
+			{
+				while (cmds[i].cmd_args[j] != NULL)
+				{
+					free(cmds[i].cmd_args[j]);
+					j++;
+				}
+				free(cmds[i].cmd_args);
+			}
+			if (cmds[i].cmd_path != NULL)
+			{
+				free(cmds[i].cmd_path);
+			}
+			i++;
+		}
+		free(cmds);
+	}
+}
--- source/token.c
+++ source/token.c
@@ -1,94 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-yamo <ael-yamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 01:52:47 by ael-yamo          #+#    #+#             */
+/*   Updated: 2022/07/01 01:53:44 by ael-yamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
-
-int	get_quote(char **str, char c)
-{
-	int	i;
-
-	i = 1;
-	while ((*str)[i] != '\0' && (*str)[i] != c)
-		i++;
-	*str = *str + i + 1;
-	return (i + 1);
-}
-
-int	get_word(char **str, int *here, int *here2)
-{
-	int	i;
-
-	i = 0;
-	if (*here)
-	{
-		while (((*str)[i] != '\0' && (*str)[i] != ' ' \
-			&& (*str)[i] != '\t' && (*str)[i] != '\v' && (*str)[i] != '\f' \
-			&& (*str)[i] != '<' && (*str)[i] != '>' && (*str)[i] != '|'
-		))
-			i++;
-		*here = 0;
-		*here2 = 1;
-	}
-	else
-	{
-		while ((*str)[i] != '\0' && ((*str)[i] != ' ' \
-		&& (*str)[i] != '\t' && (*str)[i] != '\v' && (*str)[i] != '\f' \
-		&& (*str)[i] != '<' && (*str)[i] != '>' && (*str)[i] != '\"'
-		&& (*str)[i] != '\'' && (*str)[i] != '|' && (*str)[i] != '$'
-		))
-			i++;
-	}
-	*str = *str + i;
-	return (i);
-}
-
-char	*get_char(char **str, char *c, int increment)
-{
-	*str = *str + increment;
-	return (ft_strdup(c));
-}
-
-void	get_sympol_great(t_token **tokens, char **str)
-{
-	if (**str == '>' && *(*str + 1) != '\0' && *(*str + 1) == '>')
-	{
-		add_token_last(tokens, DGREAT, ft_strdup(">>"));
-		*str = *str + 2;
-	}
-	else
-	{
-		add_token_last(tokens, GREAT, ft_strdup(">"));
-		*str = *str + 1;
-	}
-}
-
-void	get_sympol_less(t_token **tokens, char **str, int *here)
-{
-	if (**str == '<' && *(*str + 1) != '\0' && *(*str + 1) == '<')
-	{
-		add_token_last(tokens, DLESS, ft_strdup("<<"));
-		*here = 1;
-		*str = *str + 2;
-	}
-	else
-	{
-		add_token_last(tokens, LESS, ft_strdup("<"));
-		*str = *str + 1;
-	}
-}
-
-void	get_token_utils(t_token **tokens, char **str, int *here, int *here2)
-{
-	t_token	*tmp;
-
-	add_token_last(tokens, WORD, \
-	ft_substr(*str, 0, get_word(str, here, here2)));
-	if (*here2 == 1)
-	{
-		tmp = *tokens;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->old_data = ft_strdup(tmp->data);
-		*here2 = 0;
-	}
-}
 
 void	get_token(t_token **tokens, char **str)
 {
