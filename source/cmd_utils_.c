ac5b5ff388f97bf7da79302d8aa84b6ae0e0388a
Aymane El-yamoun <ael-yamo@e3r9p6.1337.ma>
2022-07-01 01:36:22 +0100

    spliting

---
+++ source/cmd_utils_.c
@@ -0,0 +1,32 @@
+#include "../includes/minishell.h"
+
+void	cmd_not_found(char *cmd)
+{
+	char	*built_in;
+
+	built_in = str_to_lower(cmd);
+	if ((ft_strcmp(built_in, "echo") && ft_strcmp(built_in, "cd") 
+        && ft_strcmp(built_in, "env") && ft_strcmp(built_in, "exit") 
+        && ft_strcmp(built_in, "export") && ft_strcmp(built_in, "pwd") 
+        && ft_strcmp(built_in, "unset")))
+		printf("minishell: %s: command not found\n", cmd);
+	free(built_in);
+}
+
+int isDirectory(const char *path) {
+   struct stat statbuf;
+   if (stat(path, &statbuf) != 0)
+       return 0;
+   return S_ISDIR(statbuf.st_mode);
+}
+
+void	is_a_dir(char *dir)
+{
+	printf("minishell: %s is a directory\n", dir);
+}
+
+char	*return_path(char **cmd_path, char **paths)
+{
+	free(paths);
+	return (*cmd_path);
+}
